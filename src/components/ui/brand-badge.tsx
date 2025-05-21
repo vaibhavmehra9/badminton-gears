2fcf8e35da3d286d07ce3db8a6f4651d044553ca
Vaibhav Mehra <vaibhavmehra@Vaibhavs-MacBook-Air.local>
2025-05-21 16:37:57 +0530

    added badmition badges

--- src/components/ui/brand-badge.tsx
+++ src/components/ui/brand-badge.tsx
@@ -1,11 +1,22 @@
 import { RacquetBrand } from "@/constants/racquet";
 import React from "react";
 import { Badge } from "./badge";
+import { cn } from "@/lib/utils";
 
 type BrandBadgeProps = {
   brand: RacquetBrand;
 };
 
 export const BrandBadge: React.FC<BrandBadgeProps> = ({ brand }) => {
-  return <Badge>{brand}</Badge>;
+  return (
+    <Badge
+      className={cn(
+        brand === RacquetBrand.YONEX && "yonex-badge",
+        brand === RacquetBrand.VICTOR && "victor-badge",
+        "text-md border-0"
+      )}
+    >
+      {brand}
+    </Badge>
+  );
 };
